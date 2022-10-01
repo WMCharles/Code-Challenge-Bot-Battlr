5cdc99b1fa078e34a6780e0190ca99f015a4dd3e
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-01 13:28:00 +0300

    add prop & handleDelete func
    the prop imports deleteBot func that updates bot state once a bot is deleted, handleDelete func deletes a bot and updates state

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,14 +1,24 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function BotCollection({data}) {
+function BotCollection({data, deleteBot}) {
   // Your code here
+  function handleDelete(item){
+    fetch(`http://localhost:8002/bots/${item.id}`, {
+      method: "DELETE",
+      headers: {
+        "content-type":"application/json"
+      }
+    })
+    .then((res) => res.json())
+    .then(() => deleteBot(item))
+  }
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
         {data.map((item) => 
-          <BotCard bot={item} key={item.id}/>
+          <BotCard bot={item} handleDelete={handleDelete} key={item.id}/>
         )}
       </div>
     </div>
