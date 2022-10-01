c37ff4d50c48a8f11dbadf8260cd392b15be1afb
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-01 22:07:04 +0300

    delete handleDelete function

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,24 +1,30 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function BotCollection({data, deleteBot}) {
+function BotCollection({data, addBot}) {
   // Your code here
-  function handleDelete(item){
-    fetch(`http://localhost:8002/bots/${item.id}`, {
-      method: "DELETE",
-      headers: {
-        "content-type":"application/json"
-      }
-    })
-    .then((res) => res.json())
-    .then(() => deleteBot(item))
+  // Discharge from army 
+  // const handleDelete = (item) => {
+  //   fetch(`http://localhost:8002/bots/${item.id}`, {
+  //     method: "DELETE",
+  //     headers: {
+  //       "content-type":"application/json"
+  //     }
+  //   })
+  //   .then((res) => res.json())
+  //   .then(() => console.log(item))
+  // }
+  // Add bot to army
+  function handleClick(item){
+    addBot(item)
   }
+
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
         {data.map((item) => 
-          <BotCard bot={item} handleDelete={handleDelete} key={item.id}/>
+          <BotCard bot={item} handleClick={handleClick} key={item.id}/>
         )}
       </div>
     </div>
