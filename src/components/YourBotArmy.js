da93c523c3e66c8761fa7d62198afa50e1e8e2df
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-01 22:05:59 +0300

    import BotCard to display army, add function to release bot from army

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,14 +1,21 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
+function YourBotArmy({army, releaseFromArmy}) {
   //your bot army code here...
 
+  // Release bot from army
+  function handleClick(item){
+    releaseFromArmy(item)
+  }
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
           {/*...and here...*/}
-          Your Bot Army
+          {army.map((item) => 
+            <BotCard bot={item} handleClick={handleClick} key={item.id}/>
+          )}
         </div>
       </div>
     </div>
