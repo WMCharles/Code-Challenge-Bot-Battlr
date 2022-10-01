5e52d7ff609d9e86fa458c45b1eb091be3322833
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-01 22:06:39 +0300

    add onclick function

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -9,13 +9,14 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-function BotCard({ bot, handleDelete }) {
+
+function BotCard({ bot, handleClick }) {
   return (
     <div className="ui column">
       <div
         className="ui card"
         key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        onClick={() => handleClick(bot)}
       >
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
@@ -47,7 +48,7 @@ function BotCard({ bot, handleDelete }) {
             <div className="ui center aligned segment basic">
               <button
                 className="ui mini red button"
-                onClick={() => handleDelete(bot)}
+                onClick={() => console.log("X clicked!!")}
               >
                 x
               </button>
